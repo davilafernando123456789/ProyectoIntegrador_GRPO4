c5888d6c179a7c501a6c78c48ac657bd37600781
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-05-08 23:59:29 -0500

    Agregando registro backend

---
+++ ProjectoInnovador_GRPO4/src/main/java/controller/SRegistro_usuario.java
@@ -0,0 +1,66 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.JOptionPane;
+
+import java.sql.*;
+
+@WebServlet("/SRegistro_producto")
+public class SRegistro_usuario extends HttpServlet {
+    public Connection conn;
+    public Statement sen;
+    public ResultSet data;
+    public String driver="com.mysql.cj.jdbc.Driver";
+    public String cadena="jdbc:mysql://localhost/ProyectoIntegrador";
+    public String usuario="root";
+    public String clave="";
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request,response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request,response);
+    }
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String codigo = request.getParameter("codigo");
+        String nombres = request.getParameter("nombres");
+        String apellidos = request.getParameter("apellidos");
+        String usuario = request.getParameter("usuario");
+        String contraseña = request.getParameter("contraseña");
+        String correo = request.getParameter("correo");
+        String telefono = request.getParameter("telefono");
+        conectar();
+        String registro;
+        try {
+        	sen = conn.createStatement();
+            registro = "insert into usuario(codigo,nombres, apellidos,usuario,contraseña, correo, telefono) "
+            		+ "values('" + codigo + "','" + nombres + "','" + apellidos + "','" + usuario + "','"+contraseña+"','"+correo+"','"+telefono+"')";
+            sen.executeUpdate(registro);
+  
+        }catch(SQLException e5) {
+            JOptionPane.showMessageDialog(null, "error en la consulta");
+        }
+        response.sendRedirect("SLista_producto");
+    }
+    public void conectar() {
+        try {
+            Class.forName(driver);
+            conn=DriverManager.getConnection(cadena,usuario,clave);
+        }catch(ClassNotFoundException e1) {
+            JOptionPane.showMessageDialog(null, "error en el driver");
+        }catch(SQLException e2) {
+            JOptionPane.showMessageDialog(null, "error en el sql");
+      }
+    }
+}
+
