85cc2c1b5a3e3bb7d6a1feecb7dbb4f597a52057
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2023-05-09 00:04:38 -0500

    Agregando registro usuario backend

--- ProjectoInnovador_GRPO4/src/main/java/controller/SRegistro_usuario.java
+++ ProjectoInnovador_GRPO4/src/main/java/controller/SRegistro_usuario.java
@@ -20,7 +20,7 @@ public class SRegistro_usuario extends HttpServlet {
     public Statement sen;
     public ResultSet data;
     public String driver="com.mysql.cj.jdbc.Driver";
-    public String cadena="jdbc:mysql://localhost/ProyectoIntegrador";///base de datos
+    public String cadena="jdbc:mysql://localhost/ProyectoIntegrador";///base de datos crearla asi
     public String usuario="root";
     public String clave="";
     
